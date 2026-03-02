6f2f22c9b23aa6f35c9e48e15fa5148c4f757bf0
abrilcrafter <abrilnunez0403@gmail.com>
2026-03-02 15:03:15 -0600

    Testing remote branch commit

---
+++ RPS_game/Class2.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+public enum handMovements
+{
+    ROCK=1,
+    PAPER=2,
+    SCISSORS=3
+}
+namespace RPS_game
+{
+    internal class Class2
+    {
+        public static void Gameplay(int userChoiceNumber)
+        {
+
+            Random random = new Random();
+
+            handMovements myHandMovement = handMovements.ROCK;
+
+            if (userChoiceNumber == (int)handMovements.ROCK)
+            {
+                myHandMovement = (handMovements)1;
+            }
+            else if (userChoiceNumber == (int)handMovements.PAPER)
+            {
+                myHandMovement = (handMovements)2;
+            }
+            else if (userChoiceNumber == (int)handMovements.SCISSORS)
+            {
+                myHandMovement = (handMovements)3;
+           
+            }
+      
+            
+            Console.WriteLine("Now its the program's choice!");
+            
+            int p_Choice = random.Next(1, 3);
+
+            Console.WriteLine(p_Choice + " vs " + myHandMovement);
+
+
+
+
+        }
+    }
+}
---
+++ RPS_game/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace RPS_game
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String userChoice;
+            Random random = new Random();
+            int randomNumberGenerator = random.Next(1, 3);
+
+
+
+            Console.WriteLine("Rock Paper Scissors!");
+            Console.WriteLine("Press any key to start...");
+
+            Console.ReadKey(true);
+
+            Console.WriteLine("\" Rock Paper Scissors! \": ");
+            Console.WriteLine();
+
+            userChoice = Class1.CheckNumbers("1 = ROCK\n2 = PAPER\n3 = SCISSORS\n");
+            int userChoiceNum = Convert.ToInt32(userChoice);
+
+            Class2.Gameplay(userChoiceNum);
+
+          //validate user input to be an integer between 1-3
+
+
+        }
+    }
+}
