1ce8a2d1d877d6ae650285e5f10ed390fa1bf7c5
abrilcrafter <abrilnunez0403@gmail.com>
2026-03-06 02:17:16 -0600

    Game now loops, display is fixed, and some validation is fixed.

---
+++ RPS_game/InputValidator.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPS_game
+{
+    internal class InputValidator
+    {
+        public static String CheckNumbers(string prompt)
+        {
+            while (true)
+            {
+               
+                Console.Write("\n" + prompt);
+
+                string numberChoice = Console.ReadLine();
+                try
+                {
+                    if (!string.IsNullOrEmpty(numberChoice))
+                    { 
+                        
+                        return numberChoice;
+                    }
+                    else
+                    {
+                        throw new ArgumentNullException("This input cannot be empty!");
+                    }
+                }
+                catch (ArgumentNullException e)
+                {
+                    {
+                        Console.WriteLine(e.Message);
+                    }
+                }
+            }
+        }
+        public static bool CheckNumberRange(int userChoiceNum)
+        {
+       
+              
+                    if (userChoiceNum < 0 || userChoiceNum > 3)
+                    {
+                   
+                    return false;   
+                  //  throw new ArgumentOutOfRangeException("Input out of range");
+                    }
+                    else
+                    {
+                    return true;
+                    }
+           
+            
+        }
+
+        public static String CheckStrings(String prompt)
+        {
+            while(true)
+            {
+                Console.WriteLine(prompt);
+                string choice = Console.ReadLine().Trim();
+
+
+                if (string.Equals(choice, "y", StringComparison.OrdinalIgnoreCase) || string.Equals(choice, "n", StringComparison.OrdinalIgnoreCase))
+                {
+                    return choice;
+                }
+                else
+                {
+                     Console.WriteLine("Please enter (y/n): " );
+                }
+  
+
+
+            }
+        }
+
+
+    }
+}
---
+++ RPS_game/RockPaperScissors_MAIN.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace RPS_game
+{
+    class RockPaperScissors_MAIN
+    {
+        static void Main(string[] args)
+        {
+           
+                String userChoice;
+                String quitOrContinue = "y";
+                Random random = new Random();
+                int randomNumberGenerator = random.Next(1, 3);
+
+
+
+                Console.WriteLine("Rock Paper Scissors!");
+                Console.WriteLine("Press any key to start...");
+
+                Console.ReadKey(true);
+
+
+                Console.WriteLine();
+
+            while (quitOrContinue == "y")
+            {
+                userChoice = InputValidator.CheckNumbers("1 = ROCK\n2 = PAPER\n3 = SCISSORS\n \n\"Rock Paper Scissors!\": ");
+                Console.WriteLine();
+                int userChoiceNum = Convert.ToInt32(userChoice);
+               
+
+                //bool checkResult =  InputValidator.CheckNumberRange(userChoiceNum);
+                //if (!checkResult)
+                //{
+                //    Console.WriteLine("Input out of range");
+                //    return;
+                //}
+
+                GameDisplay.Gameplay(userChoiceNum);
+                quitOrContinue = InputValidator.CheckStrings("Continue game? (y/n)");
+
+             
+               
+            } //while true 
+   Console.WriteLine("Bye Thank you for playing!");
+        }
+    }
+}
